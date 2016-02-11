3d16e019fea96620015505bac9a5e94394d9c358
kayuda91 <kayuda91@gmail.com>
2016-02-11 12:05:18 +0300

    Оптимизация изображений и загрузки, меню для моб

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-jQuery(document).ready(function () {
-    var $ = jQuery;
-    $("head").append('<link rel="stylesheet" href="/css/normalize.min.css" >');
-    $("head").append('<link rel="stylesheet" href="/css/font-awesome.min.css">');
-    $("head").append('<link rel="stylesheet" href="css/style.css">');
-    $('.caption').click(function(){
-        $(this).hide();
-    });
-});
+jQuery(document).ready(function(){var $=jQuery;$("head").append('<link rel="stylesheet" href="./css/normalize.min.css" >');$("head").append('<link rel="stylesheet" href="./css/font-awesome.min.css">');$("head").append('<link rel="stylesheet" href="./css/style.css">');$('.caption').click(function(){$(this).hide();});});
